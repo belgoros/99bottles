a6d2dafbe66eec809c837136d1dae85e7c69f402
Serguei Cambour <external.z28scamb@decathlon.com>
2017-03-07 10:04:00 +0100

    Use metaprogramming to replace case/when statement

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -21,14 +21,9 @@ class BottleNumber
   attr_reader :number
 
   def self.for(number)
-    case number
-    when 0
-      BottleNumber0
-    when 1
-      BottleNumber1
-    when 6
-      BottleNumber6
-    else
+    begin
+      const_get("BottleNumber#{number}")
+    rescue NameError
       BottleNumber
     end.new(number)
   end
